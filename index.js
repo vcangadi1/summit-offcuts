34582a49acdd6d1370fa3246850958feb49fa223
Veerendra Angadi <Angadi_V@wmgds.wmg.warwick.ac.uk>
2022-04-14 16:45:46 +0100

    connected to google-spreadsheet

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const dotenv = require('dotenv');
 const path = require('path');
+const { GoogleSpreadsheet } = require('google-spreadsheet');
+const credentials = require('./private/credentials.json');
 
 const app = express();
 
@@ -11,7 +13,12 @@ const ip = process.env.ip || '0.0.0.0';
 
 const publicPath = path.join(__dirname, './public');
 app.use(express.static(publicPath));
-
+app.use( (req, res, next) => {
+  if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
+    return res.sendStatus(204);
+  }
+  return next();
+});
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
@@ -21,18 +28,42 @@ app.get('/', (req, res) => {
   res.render('index');
 });
 
-app.post('/', (req, res) => {
-    const { material, size, task, qty } = req.body;
-    console.log(req.body);
+app.post('/', async (req, res) => {
 
-    if (material === undefined || size === undefined || task === undefined || qty === '0') {
-      return res.render('index', {
-          message: 'Please select all entities',
-          color: 'danger'
-      });
-    } else {
+  // Initialize the spreadsheet
+  const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
+  // Authenticate with the Google Spreadsheet
+  await doc.useServiceAccountAuth(credentials);
+  await doc.loadInfo();
+  // Get the first sheet
+  const sheet = doc.sheetsByTitle[process.env.SHEET_NAME];
+  // Get all the rows
+  await sheet.loadCells('A2:B2');
+  console.log(sheet.cellStats);
+
+
+  const { material, size, task, qty } = req.body;
+  console.log(req.body);
+
+  if (material === undefined || size === undefined || task === undefined || qty === '0') {
+    return res.render('index', {
+        message: 'Please select all entities',
+        color: 'danger'
+    });
+  } else {
+    try {
+      let A2 = Number(sheet.getCellByA1('A2').value);
+      A2 += Number(qty);
+      sheet.getCellByA1('A2').value = A2;
+      await sheet.saveUpdatedCells();
+    }
+    catch {
+      
+    } finally {
       return res.render('index');
     }
+
+  }
 });
 
 app.listen(port, ip, () => {
