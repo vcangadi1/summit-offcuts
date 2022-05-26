aa7d8dc90ff3fb65e01e3c069d95645f6623b9fb
vcangadi1 <veer.angadi@warwick.ac.uk>
2022-05-26 10:22:06 +0100

    update with materials

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const path = require('path');
 const { GoogleSpreadsheet } = require('google-spreadsheet');
 const credentials = require('./private/credentials.json');
+const async = require('hbs/lib/async');
 
 const app = express();
 
@@ -24,11 +25,27 @@ app.use(express.json());
 
 app.set('view engine', 'hbs');
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
+  // Initialize the spreadsheet
+  const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
+  // Authenticate with the Google Spreadsheet
+  await doc.useServiceAccountAuth(credentials);
+  await doc.loadInfo();
+  // Get the first sheet
+  const sheet = doc.sheetsByTitle[process.env.SHEET_NAME];
+  // Get all the rows
+  const rows = await sheet.getRows();
+  let values = [];
+  let text = '';
+  rows.forEach(async (row) => {
+    
+  });
   res.render('index');
 });
 
 app.post('/', async (req, res) => {
+
+  const { Code, size, task, qty } = req.body;
 
   // Initialize the spreadsheet
   const doc = new GoogleSpreadsheet(process.env.SPREADSHEET_ID);
@@ -38,55 +55,31 @@ app.post('/', async (req, res) => {
   // Get the first sheet
   const sheet = doc.sheetsByTitle[process.env.SHEET_NAME];
   // Get all the rows
-  await sheet.loadCells('B2:C3');
-
-  const { material, size, task, qty } = req.body;
-  console.log(req.body);
-
-  if (material === undefined || size === undefined || task === undefined || qty === '0') {
+  const rows = await sheet.getRows();
+  // Check if all entries are valid
+  if (Code === 'selected' || size === undefined || task === undefined || qty === '0') {
     return res.render('index', {
         message: 'Please select all entities',
         color: 'danger'
     });
-  } else {
-    try {
-      if (material === 'aluminium') {
-        if (size === 'full') {
-          if (task === 'return') {
-            sheet.getCellByA1('B2').value = parseInt(sheet.getCellByA1('B2').value) + parseInt(qty);
-          } else if (task === 'consume') {
-            sheet.getCellByA1('B2').value = parseInt(sheet.getCellByA1('B2').value) - parseInt(qty);
-          }
-        } else if (size === 'half') {
-          if (task === 'return') {
-            sheet.getCellByA1('C2').value = parseInt(sheet.getCellByA1('C2').value) + parseInt(qty);
-          } else if (task === 'consume') {
-            sheet.getCellByA1('C2').value = parseInt(sheet.getCellByA1('C2').value) - parseInt(qty);
-          }
-        }
-      } else if (material === 'wood') {
-        if (size === 'full') {
-          if (task === 'return') {
-            sheet.getCellByA1('B3').value = parseInt(sheet.getCellByA1('B3').value) + parseInt(qty);
-          } else if (task === 'consume') {
-            sheet.getCellByA1('B3').value = parseInt(sheet.getCellByA1('B3').value) - parseInt(qty);
-          }
-        } else if (size === 'half') {
-          if (task === 'return') {
-            sheet.getCellByA1('C3').value = parseInt(sheet.getCellByA1('C3').value) + parseInt(qty);
-          } else if (task === 'consume') {
-            sheet.getCellByA1('C3').value = parseInt(sheet.getCellByA1('C3').value) - parseInt(qty);
-          }
-        }
+  }
+  // Find the row with the code
+  if (rows.length) {
+    rows.forEach( async(item) => {
+      if (item.Code === Code) {
+        if (task === 'return' && size === 'Full') { item.Full = parseInt(item.Full) + parseInt(qty); }
+        if (task === 'return' && size === 'Half') { item.Half = parseInt(item.Half) + parseInt(qty); }
+        if (task === 'consume' && size === 'Full') { item.Full = parseInt(item.Full) - parseInt(qty); }
+        if (task === 'consume' && size === 'Half') { item.Half = parseInt(item.Half) - parseInt(qty); }
+        await item.save();
+        return res.render('index');
       }
-      // Save the changes
-      await sheet.saveUpdatedCells();
-    }
-    catch {} 
-    finally {
-      return res.render('index');
-    }
-
+    });
+  } else {
+    return res.render('index', {
+      message: 'Empty database',
+      color: 'danger'
+    });
   }
 });
 
